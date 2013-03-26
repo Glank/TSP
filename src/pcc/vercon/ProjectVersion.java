3c0c6216c9fb5d7d850e2ac3e7db6058a69e8f6e
jaiok <jaiok@hotmail.com>
2013-03-26 15:31:44 -0300

    Latest changes after testing driver

--- src/pcc/vercon/ProjectVersion.java
+++ src/pcc/vercon/ProjectVersion.java
@@ -1,6 +1,9 @@
 package pcc.vercon;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Date;
+import java.util.Iterator;
 
 
 public class ProjectVersion implements java.io.Serializable
@@ -16,6 +19,7 @@ public class ProjectVersion implements java.io.Serializable
 		m_number	=	(null == number) ? "None" : number;
 		m_author	=	(null == author) ? "None": author;
 		m_reason	=	(null == reason) ? "None" : reason;
+		m_files		=	new ArrayList<SourceFileRecord>();
 		
 		for (int ii = 0; ii < filenames.size(); ii ++)
 		{
@@ -69,4 +73,70 @@ public class ProjectVersion implements java.io.Serializable
 		
 		return (ret);
 	}
+	
+	public boolean setLinesForFile (String fileName, String [] lines)
+	{
+		boolean				found			=	false;
+		
+		if (fileName == null || fileName.isEmpty())
+		{
+			return found;
+		}
+		
+		//
+		// Find the src-file-record for this file-name and set these lines for that.
+		// --> this can be in the constructor itself where it can take a hashmap of filename to lines of code.
+		//
+		SourceFileRecord	thisSrcFileRec	=	null;
+		
+		for (int ii = 0; ii < m_files.size(); ii ++)
+		{
+			if ( (thisSrcFileRec = m_files.get(ii)).getName().equalsIgnoreCase(fileName))
+			{
+				thisSrcFileRec.setLines(lines);
+				
+				found	=	true;
+
+				break;
+				
+			}
+		}
+
+		return (found);
+	}
+	
+	public boolean setLinesForFiles ( HashMap<String, String []> filesAndLines)
+	{
+		boolean				found			=	false;
+		
+		if (filesAndLines == null || filesAndLines.isEmpty())
+		{
+			return found;
+		}
+		
+		//
+		// Find the src-file-record for this file-name and set these lines for that.
+		// --> this can be in the constructor itself where it can take a hashmap of filename to lines of code.
+		//
+		String				curFileName		=	null;
+		String[]			curLines		=	null;
+		
+		for (Map.Entry<String, String[]> entry : filesAndLines.entrySet())
+		{
+	
+			curFileName	=	entry.getKey();
+			curLines	=	entry.getValue();
+
+			// Very unoptimized - but good enough for now..
+			setLinesForFile(curFileName, curLines);
+		}
+
+		if (null != curFileName)
+		{
+			found	=	true;
+		}
+		
+		return (found);
+	}
+	
 }
