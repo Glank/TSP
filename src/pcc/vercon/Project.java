c147b52c7d918d4bc4444bbeb7b58943db772dad
jaiok <jaiok@hotmail.com>
2013-03-26 13:34:25 -0300

    Filled in skeleton code

--- src/pcc/vercon/Project.java
+++ src/pcc/vercon/Project.java
@@ -10,32 +10,67 @@ public class Project implements java.io.Serializable{
 	 */
 	private ArrayList<String> versions;
 	private ArrayList<String> sourceFileNames;
-	public  Project(){
-		//TODO
+	
+	private ArrayList<ProjectVersion>	m_projVersions;
+	
+	public  Project()
+	{
+		versions		=	new ArrayList();
+		sourceFileNames	=	new ArrayList();
+		m_projVersions	=	new ArrayList();
+		
 	}
-	public void commit(String number, String author, String reason){
-		//TODO
+	
+	public void commit(String number, String author, String reason)
+	{
+		ProjectVersion	projVersion		=	new ProjectVersion (number, author, reason, sourceFileNames);
+		
+		m_projVersions.add(projVersion);
+		
+		versions.add(number);
+		latestVersion	=	number;
+
 	}
-	public void addFile(String filename){
-		//TODO
+	
+	public void addFile(String filename)
+	{
+		sourceFileNames.add(filename);
 	}
-	public void removeFile(String filename){
-		//TODO
+	
+	public void removeFile(String filename)
+	{
+		sourceFileNames.remove(filename);
 	}
-	public ArrayList<String> getFiles(){
-		//TODO
-		return null;
+	
+	public ArrayList<String> getFiles()
+	{
+		return (sourceFileNames);
 	}
-	public String getCurrentVersion(){
-		//TODO
-		return null;
+	
+	public String getCurrentVersion()
+	{
+		return latestVersion;
 	}
-	public ProjectVersion getVersion(String number){
-		//TODO
-		return null;
+	
+	public ProjectVersion getVersion(String number)
+	{
+		ProjectVersion		retProjVersion	=	null;
+		
+		for (int ii = 0; ii < m_projVersions.size(); ii ++)
+		{
+			if (m_projVersions.get(ii).getNumber().equalsIgnoreCase(number))
+			{
+				retProjVersion	=	m_projVersions.get(ii);
+				break;
+			}
+		}
+			
+
+		return retProjVersion;
 	}
-	public ArrayList<String> getVersionList(){
-		//TODO
-		return null;
+
+	public ArrayList<String> getVersionList()
+	{
+		return (versions);
 	}
 }
