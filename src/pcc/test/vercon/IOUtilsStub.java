264a24fc8efdb6cb63156753cb6a88e9b079166b
Jai <jaiok@hotmail.com>
2013-03-26 14:18:29 -0400

    adding vercon test

---
+++ src/pcc/test/vercon/IOUtilsStub.java
@@ -0,0 +1,36 @@
+package pcc.test.vercon;
+import pcc.vercon.Project;
+import pcc.vercon.ProjectVersion;
+
+
+public class IOUtilsStub{
+/*
+	public static Project openProject(String filename){
+		//TODO
+		return null;
+	}
+	public static void saveProject(Project project, String filename){
+		//TODO
+	}
+	public static ProjectVersion openVersion(String filename){
+		//TODO
+		return null;
+	}
+	public static void saveVersion(Project project, String filename){
+		//TODO
+	}
+	public static String[] openSourceFile(String filename){
+		//TODO
+		return null;
+	}
+	public static void writeSourceFile(String filename, String contents){
+		System.out.println("Writing: "+filename);
+		System.out.println("################");
+		System.out.println(contents);
+		System.out.println("################");
+	}
+	public static void createFolder(String name){
+		System.out.println("MkDir: " + name);
+	}
+//*/
+}
